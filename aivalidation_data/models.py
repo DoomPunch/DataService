7b319c920c8c46011ff5c9483b1714a5bc6ce273
ff <punchdoom@gmail.com>
2022-07-15 17:23:19 +0800

    model格式修改

--- aivalidation_data/models.py
+++ aivalidation_data/models.py
@@ -13,17 +13,58 @@ class InfinityData(models.Model):
 
 
 class Item(models.Model):
-    item_code_lis = models.CharField(max_length=10, db_column='ItemCodeLis', null=True, blank=True)
-    item_code_middleware = models.CharField(max_length=10, db_column='ItemCodeLis', null=True, blank=True)
+    item_code_lis = models.CharField(
+        'lis_code',
+        max_length=10,
+        db_column='ItemCodeLis',
+        null=True,
+        blank=True
+    )
+    item_code_middleware = models.CharField(
+        'infinity_code',
+        max_length=10,
+        db_column='ItemCodeLis',
+        null=True,
+        blank=True
+    )
 
 
 class ItemRange(models.Model):
-    item = models.ForeignKey(Item, on_delete=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    max_range = models.FloatField()
+    min_range = models.FloatField()
 
 
 class Patient(models.Model):
-    name = models.CharField(max_length=10, db_column='Name', null=True, blank=True)
-    birthday = models.DateField(db_column='Birthday', null=True, blank=True)
-    create_time = models.DateTimeField(db_column='CreateTime', default=timezone.now, null=True, blank=True)
-    modified_by = models.IntegerField(db_column='ModifiedBy', null=True, blank=True)
-    modified_time = models.DateTimeField(db_column='ModifiedTime', auto_now=True, null=True, blank=True)
+    name = models.CharField(
+        max_length=10,
+        db_column='Name',
+        null=True,
+        blank=True
+    )
+
+    birthday = models.DateField(
+        db_column='Birthday',
+        null=True,
+        blank=True
+    )
+
+    create_time = models.DateTimeField(
+        db_column='CreateTime',
+        default=timezone.now,
+        null=True,
+        blank=True
+    )
+
+    modified_by = models.IntegerField(
+        db_column='ModifiedBy',
+        null=True,
+        blank=True
+    )
+
+    modified_time = models.DateTimeField(
+        db_column='ModifiedTime',
+        auto_now=True,
+        null=True,
+        blank=True
+    )
